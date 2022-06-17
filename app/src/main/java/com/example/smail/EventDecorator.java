fdccc5b93847ac9f8afd0996d89c5095e0a6039c
calia07 <calia_ksh@naver.com>
2022-06-18 05:04:01 +0900

    다이어리 저장 기능

---
+++ app/src/main/java/com/example/smail/EventDecorator.java
@@ -0,0 +1,41 @@
+package com.example.smail;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class EventDecorator implements DayViewDecorator {
+    private final int color;
+    private final HashSet<CalendarDay> dates;
+
+    public EventDecorator(int color, Collection<CalendarDay> dates){
+        this.color = color;
+        this.dates = new HashSet<>(dates);
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return dates.contains(day);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.addSpan(new StyleSpan(Typeface.BOLD));
+        view.addSpan(new RelativeSizeSpan(1f));
+        view.addSpan(new ForegroundColorSpan(Color.YELLOW));
+        view.addSpan(new BackgroundColorSpan(Color.RED));
+        view.addSpan(new DotSpan(10,color));
+
+    }
+}
--- app/src/main/java/com/example/smail/tabFragment/calender.java
+++ app/src/main/java/com/example/smail/tabFragment/calender.java
@@ -29,28 +29,225 @@
 //    }
 //}
 package com.example.smail.tabFragment;
+import static android.content.Context.MODE_NO_LOCALIZED_COLLATORS;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
+import com.example.smail.CalendarTab;
+import com.example.smail.EventDecorator;
+import com.example.smail.MainActivity;
 import com.example.smail.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Collection;
+import java.util.Collections;
 
 
 public class calender extends Fragment {
 
-
-    public calender() {
-        // Required empty public constructor
-    }
+    public String fname=null;
+    public String str=null;
+    public MaterialCalendarView calendarView;
+    public Button cha_Btn,del_Btn,save_Btn;
+    public TextView diaryTextView,textView2,textView3;
+    public EditText contextEditText;
+    public CalendarDay calendarDay;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.calender, container, false);
+        View view = inflater.inflate(R.layout.calender, container, false);
+
+        calendarView=view.findViewById(R.id.calendarView);
+        diaryTextView=view.findViewById(R.id.diaryTextView);
+        save_Btn=view.findViewById(R.id.save_Btn);
+        del_Btn=view.findViewById(R.id.del_Btn);
+        cha_Btn=view.findViewById(R.id.cha_Btn);
+        textView2=view.findViewById(R.id.textView2);
+        //textView3=view.findViewById(R.id.textView3);
+        contextEditText=view.findViewById(R.id.contextEditText);
+        //로그인 및 회원가입 엑티비티에서 이름을 받아옴
+
+
+        recycle();
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                calendarDay = date;
+                diaryTextView.setVisibility(View.VISIBLE);
+                save_Btn.setVisibility(View.VISIBLE);
+                contextEditText.setVisibility(View.VISIBLE);
+                textView2.setVisibility(View.INVISIBLE);
+                cha_Btn.setVisibility(View.INVISIBLE);
+                del_Btn.setVisibility(View.INVISIBLE);
+                diaryTextView.setText(String.format("%d / %d / %d",date.getYear(),date.getMonth()+1,date.getDay()));
+                contextEditText.setText("");
+                checkDay(date.getYear(),date.getMonth(),date.getDay());
+            }
+        });
+        save_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveDiary(fname);
+                str=contextEditText.getText().toString();
+                textView2.setText(str);
+                save_Btn.setVisibility(View.INVISIBLE);
+                cha_Btn.setVisibility(View.VISIBLE);
+                del_Btn.setVisibility(View.VISIBLE);
+                contextEditText.setVisibility(View.INVISIBLE);
+                textView2.setVisibility(View.VISIBLE);
+
+            }
+        });
+        return view;
+    }
+
+    public void recycle(){
+        int cYear=2022;
+        int cMonth,cDay;
+        for(int i = 1;i<12;i++){
+            for(int j=1;j<29;j++){
+                cMonth = i;
+                cDay = j;
+                fname=""+cYear+"-"+(cMonth+1)+""+"-"+cDay+".txt";//저장할 파일 이름설정
+                FileInputStream fis=null;//FileStream fis 변수
+
+                try{
+
+                    fis=getActivity().openFileInput(fname);
+
+                    byte[] fileData=new byte[fis.available()];
+                    fis.read(fileData);
+                    fis.close();
+
+                    str=new String(fileData);
+                    if(str != null){
+                        CalendarDay calendarDay1 = new CalendarDay(cYear,cMonth,cDay);
+                        calendarView.addDecorator(new EventDecorator(Color.YELLOW, Collections.singleton(calendarDay1)));
+                    }
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public void  checkDay(int cYear,int cMonth,int cDay){
+        fname=""+cYear+"-"+(cMonth+1)+""+"-"+cDay+".txt";//저장할 파일 이름설정
+        FileInputStream fis=null;//FileStream fis 변수
+
+        try{
+
+            fis=getActivity().openFileInput(fname);
+
+            byte[] fileData=new byte[fis.available()];
+            fis.read(fileData);
+            fis.close();
+
+            str=new String(fileData);
+
+            contextEditText.setVisibility(View.INVISIBLE);
+            textView2.setVisibility(View.VISIBLE);
+            calendarView.addDecorator(new EventDecorator(Color.YELLOW, Collections.singleton(calendarDay)));
+            textView2.setText(str);
+
+            save_Btn.setVisibility(View.INVISIBLE);
+            cha_Btn.setVisibility(View.VISIBLE);
+            del_Btn.setVisibility(View.VISIBLE);
+
+            cha_Btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    contextEditText.setVisibility(View.VISIBLE);
+                    textView2.setVisibility(View.INVISIBLE);
+                    contextEditText.setText(str);
+
+                    save_Btn.setVisibility(View.VISIBLE);
+                    cha_Btn.setVisibility(View.INVISIBLE);
+                    del_Btn.setVisibility(View.INVISIBLE);
+                    textView2.setText(contextEditText.getText());
+                }
+
+            });
+            del_Btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    textView2.setVisibility(View.INVISIBLE);
+                    contextEditText.setText("");
+                    contextEditText.setVisibility(View.VISIBLE);
+                    save_Btn.setVisibility(View.VISIBLE);
+                    cha_Btn.setVisibility(View.INVISIBLE);
+                    del_Btn.setVisibility(View.INVISIBLE);
+                    removeDiary(fname);
+                }
+            });
+            if(textView2.getText()==null){
+                textView2.setVisibility(View.INVISIBLE);
+                diaryTextView.setVisibility(View.VISIBLE);
+                save_Btn.setVisibility(View.VISIBLE);
+                cha_Btn.setVisibility(View.INVISIBLE);
+                del_Btn.setVisibility(View.INVISIBLE);
+                contextEditText.setVisibility(View.VISIBLE);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @SuppressLint("WrongConstant")
+    public void removeDiary(String readDay){
+
+        FileOutputStream fos=null;
+
+        try{
+            fos=getActivity().openFileOutput(readDay,MODE_NO_LOCALIZED_COLLATORS);
+            String content="";
+            fos.write((content).getBytes());
+            fos.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @SuppressLint("WrongConstant")
+    public void saveDiary(String readDay){
+        FileOutputStream fos=null;
+        calendarView.addDecorator(new EventDecorator(Color.YELLOW, Collections.singleton(calendarDay)));
+
+        try{
+            fos=getActivity().openFileOutput(readDay,MODE_NO_LOCALIZED_COLLATORS);
+            String content=contextEditText.getText().toString();
+            fos.write((content).getBytes());
+            fos.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
+
